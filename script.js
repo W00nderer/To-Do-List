2f22e7507cde3a25fe95edec6b07dc0639dd6b07
W00nderer <lsalimullina0311@gmail.com>
2024-04-14 20:02:40 +0900

    First Draft
    FIrst day of working on the to-do list website. Please submit your suggestions, it will help a lot!

---
+++ script.js
@@ -0,0 +1,103 @@
+//STICKY BALLS ANIMATION
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext('2d');
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+ctx.fillStyle = 'pink';
+
+class Ball {
+  constructor(effect){
+    this.effect = effect;
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.radius = Math.random()*60 + 20;
+    this.speedX = Math.random() - 0.5;
+    this.speedY = Math.random() - 0.5;
+  }
+  update(){
+    if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+      this.speedX = -this.speedX;
+    }
+    if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+      this.speedY = -this.speedY;
+    }
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+  draw(context){
+    context.beginPath();
+    context.arc(this.x,this.y,this.radius,0,Math.PI*2);
+    context.fill();
+  }
+  reset(){
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+  }
+}
+class MetaballEffect{
+  constructor(width, height){
+    this.width = width;
+    this.height = height;
+    this.metaballsArray = [];
+  }
+  init(numberOfBalls){
+    for( let i=0; i < numberOfBalls; i++){
+      this.metaballsArray.push(new Ball(this));
+    }
+  }
+  update(){
+    this.metaballsArray.forEach(metaball => metaball.update());
+  }
+  draw(context){
+    this.metaballsArray.forEach(metaball => metaball.draw(context));
+  }
+  reset(newWidth, newHeight){
+    this.width = newWidth;
+    this.height = newHeight;
+    this.metaballsArray.forEach(metaball => metaball.reset());
+  }
+}
+
+const effect = new MetaballEffect(canvas.width, canvas.height);
+effect.init(50);
+console.log(effect);
+
+function animate(){
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+  effect.update();
+  effect.draw(ctx);
+  requestAnimationFrame(animate);
+}
+animate();
+
+window.addEventListener('resize', function(){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.fillStyle = 'pink';
+  effect.reset(canvas.width,canvas.height);
+})
+
+
+//MOVING SECTION
+
+const movingSection = document.getElementById("intro-section");
+const h1 = document.getElementById("h1");
+let checkpoint1 = 0;
+
+window.addEventListener('scroll', moveSection);
+
+
+function moveSection(){
+  let scrollPosition = window.scrollY;
+  if(scrollPosition>=200){
+    movingSection.style.animation = "change .2s ease-in-out forwards";
+    checkpoint1=1;
+    h1.style.animation = "h1-change .2s ease-in-out forwards"
+  }
+  if(scrollPosition<=20 && checkpoint1===1){
+    movingSection.style.animation = "change-back .2s ease-in-out forwards";
+    h1.style.animation = "h1-change-back .2s ease-in-out forwards"
+    checkpoint1=0;
+  }
+}
